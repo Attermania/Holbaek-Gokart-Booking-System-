8fcee03d2119f80f18c7fcffbd3081ab0e37fed2
Thomas Falk Attermann <thomas_5009@hotmail.com>
2015-05-05 15:38:56 +0200

    add gokart booking window

---
+++ src/main/java/dk/gokartland/booking/controllers/BookingController.java
@@ -0,0 +1,39 @@
+package dk.gokartland.booking.controllers;
+
+import dk.gokartland.booking.factories.FXMLFactory;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Thomas on 05-05-2015.
+ */
+public class BookingController implements Initializable {
+
+    @FXML
+    Button addGokartButton;
+
+    private FXMLFactory fxmlFactory;
+
+    public BookingController(FXMLFactory fxmlFactory) {
+        this.fxmlFactory = fxmlFactory;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        addGokartButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Stage stage = fxmlFactory.build(getClass().getResource("addGokart.fxml"));
+                stage.show();
+            }
+        });
+
+    }
+}
